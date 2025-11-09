8003d0f44c1e804bab26d57e4fe83695af282736
Pablo Henrique <pablo.henriquecba1@gmail.com>
2025-11-09 00:00:27 -0300

    integrando model de empresas

--- core/apps/empresas/models/models.py
+++ core/apps/empresas/models/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Empresa(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    endereco = models.CharField(max_length=225)
+    telefone = models.IntegerField(max_length=20)
+    email = models.EmailField(unique=True)
+    dono = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='empresas'
+    )
+    servicos = models.ManyToManyField('Servico', related_name='empresas')
+
+    def __str__(self):
+        return self.nome
+    
