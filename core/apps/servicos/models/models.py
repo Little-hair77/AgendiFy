a58d94af2928b7138e86c2792de2edc94921221c
Pablo Henrique <pablo.henriquecba1@gmail.com>
2025-11-09 00:02:33 -0300

    models de servicos

--- core/apps/servicos/models/models.py
+++ core/apps/servicos/models/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Servico(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
