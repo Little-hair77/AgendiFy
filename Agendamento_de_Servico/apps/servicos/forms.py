181d5bf59145ceef6b121c5144f26bf0cd528dd8
Little-hair77 <pablo.henriquecba1@gmail.com>
2025-11-13 16:00:55 -0300

    forms do app "servicos"

--- Agendamento_de_Servico/apps/servicos/forms.py
+++ Agendamento_de_Servico/apps/servicos/forms.py
@@ -6,5 +6,25 @@ class ServicoForm(forms.ModelForm):
         model = Servico
         fields = ['empresa', 'nome', 'descricao', 'preco']
         widgets = {
-            
+            'empresa': forms.Select(attrs={'class': 'form-select'}),
+            'nome': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Nome do serviço'
+            }),
+            'descricao': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Descreva o serviço oferecido'
+            }),
+            'descricao': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Descreva o serviço oferecido'
+            }),
+        }
+        labels = {
+            'empresa': 'Empresa responsável',
+            'nome': 'Nome do serviço',
+            'descricao': 'Descrição',
+            'preco': 'Preço (R$)',
         }
