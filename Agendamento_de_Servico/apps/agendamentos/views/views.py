8b775bf5b43018faa1564802036a0ce65aebc489
Pablo Henrique <pablo.henriquecba1@gmail.com>
2025-11-16 23:47:55 -0300

    editando app "agendamentos"

--- Agendamento_de_Servico/apps/agendamentos/views/views.py
+++ Agendamento_de_Servico/apps/agendamentos/views/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from models import Agendamento
 
 # Create your views here.
