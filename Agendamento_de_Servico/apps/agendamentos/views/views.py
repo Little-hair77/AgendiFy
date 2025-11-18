bdcb422c96bdc12e3004cdaec7e41886e3541e8b
Little-hair77 <pablo.henriquecba1@gmail.com>
2025-11-17 23:13:50 -0300

    editar agendamentos e verificação de login

--- Agendamento_de_Servico/apps/agendamentos/views/views.py
+++ Agendamento_de_Servico/apps/agendamentos/views/views.py
@@ -4,10 +4,12 @@ from models import Agendamento
 from forms import AgendamentoForm
 
 # Create your views here.
+@login_required
 def listar_agendamentos(request):
     agendamentos = Agendamento.objects.all()
     return render(request, 'agendamentos/listar.html', {'agendamentos': agendamentos})
 
+@login_required
 def criar_agendamentos(request):
     if request.method == 'POST':
         form = AgendamentoForm(request.POST)
@@ -22,3 +24,18 @@ def criar_agendamentos(request):
             form = AgendamentoForm()
     
     return render(request, 'agendamentos/criar.html', {'form': form})
+
+@login_required
+def editar_agendamento(request):
+    agendamento = get_object_or_404(Agendamento, id=id, usuario=request.user)
+   
+    if request.method == 'POST':
+        form = Agendamento(request.POST, instance=agendamento)
+
+        if form.is_valid():
+            form.save()
+            return redirect('listar_agendamentos')
+        else:
+            form = AgendamentoForm(instance=agendamento)
+
+    return render(request, 'agendamentos/editar.html', {'form': form, 'agendamento': agendamento})
