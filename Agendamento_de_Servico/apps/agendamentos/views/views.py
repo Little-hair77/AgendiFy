83fcaa9f4801e4a8ce91f6c09effd4d663fae036
Little-hair77 <pablo.henriquecba1@gmail.com>
2025-11-17 23:08:28 -0300

    listar e criar agendamento, feito

--- Agendamento_de_Servico/apps/agendamentos/views/views.py
+++ Agendamento_de_Servico/apps/agendamentos/views/views.py
@@ -4,4 +4,21 @@ from models import Agendamento
 from forms import AgendamentoForm
 
 # Create your views here.
+def listar_agendamentos(request):
+    agendamentos = Agendamento.objects.all()
+    return render(request, 'agendamentos/listar.html', {'agendamentos': agendamentos})
 
+def criar_agendamentos(request):
+    if request.method == 'POST':
+        form = AgendamentoForm(request.POST)
+
+        if form.is_valid():
+            agendamento = form.save(commit=False)
+            agendamento.usuario = request.user
+            agendamento.save()
+
+            return redirect('listar_agendamentos')
+        else:
+            form = AgendamentoForm()
+    
+    return render(request, 'agendamentos/criar.html', {'form': form})
