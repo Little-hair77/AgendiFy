5c62d563d8bbe27955fd819ed047694049115998
Little-hair77 <pablo.henriquecba1@gmail.com>
2025-11-18 01:27:31 -0300

    view de servicos

--- Agendamento_de_Servico/apps/servicos/views/views.py
+++ Agendamento_de_Servico/apps/servicos/views/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from models import Servico
+from forms import ServicoForm
 
 # Create your views here.
+def listar_servicos(request):
+    servicos = Servico.objects.all().order_by('nome')
+
+    return render(request, 'servicos/listar.html', {'servicos': servicos})
+
+def criar_servico(request):
+    if request.method == 'POST':
+        form = ServicoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('listar_servicos')
+        else:
+            form = ServicoForm()
+
+    return render(request, 'servicos/listar.html', {'form': form})
+
+def editar_servico(request, id):
+    servico = get_object_or_404(Servico, id=id)
+
+    if request.method == 'POST':
+        form = ServicoForm(request.POST, instance=servico)
+
+        if form.is_valid():
+            form.save()
+            return redirect('listar_servicos')
+        else:
+            form = ServicoForm(instance=servico)
+
+    return render(request, 'servicos/editar.html', {'form': form, 'servico': servico})
+
+def excluir_servico(request, id):
+    servico = get_object_or_404(Servico, id=id)
+    servico.delete()
+
+    return redirect('listar_servicos')
+
+def detalhes_servico(request, id):
+    servico = get_object_or_404(Servico, id)
+
+    return render(request, 'servicos/detalhes.html', {'servico': servico})
