48c8f014713014ab6152618f031eb4b670bad60e
Little-hair77 <pablo.henriquecba1@gmail.com>
2025-11-18 02:42:51 -0300

    urls do core

--- Agendamento_de_Servico/core/urls.py
+++ Agendamento_de_Servico/core/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Apps
+    path('usuarios/', include('apps.usuarios.urls')),
+    path('empresas/', include('apps.empresas.urls')),
+    path('servicos/', include('apps.servicos.urls')),
+    path('agendamentos/', include('apps.agendamentos.urls')),
+
 ]
