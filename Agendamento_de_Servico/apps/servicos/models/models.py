d678d14e697affacf577dd5562bb4579c4eefba3
Little-hair77 <pablo.henriquecba1@gmail.com>
2025-11-21 12:12:45 -0300

    novas migrates

--- Agendamento_de_Servico/apps/servicos/models/models.py
+++ Agendamento_de_Servico/apps/servicos/models/models.py
@@ -13,7 +13,7 @@ class Servico(models.Model):
     descricao = models.TextField(blank=True)
     preco = models.DecimalField(max_digits=8, decimal_places=2)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name='servicos')
-    tag = models.ManyToManyField(Tag, blank=True, related_name='servicos')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='servicos')
 
     def __str__(self):
         return self.nome
