493b82128daefd8f61cddf4cd99aa4be49073310
Little-hair77 <pablo.henriquecba1@gmail.com>
2025-11-25 21:38:33 -0300

    urls app "profissionais"

--- Agendamento_de_Servico/apps/profissionais/urls.py
+++ Agendamento_de_Servico/apps/profissionais/urls.py
@@ -1,1 +1,11 @@
 from django.urls import path
+from .views import views
+
+app_name = 'profissionais'
+
+urlpatters = [
+    path('', views.listar_profissionais, name='listar_profissionais'),
+    path('novo/', views.cadastrar_profissional, name='cadastrar_profissional'),
+    path('<int:pk>/editar/', views.editar_profissional, name='editar_profissional'),
+    path('<int:pk>/deletar/', views.deletar_profissional, name='deletar_profissional'),
+]
