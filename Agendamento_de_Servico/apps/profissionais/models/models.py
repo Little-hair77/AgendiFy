a1c010fdb7b83a52126bf3e6320cc35aeafe30f6
Little-hair77 <pablo.henriquecba1@gmail.com>
2025-11-25 21:10:05 -0300

    Novo app com models

---
+++ Agendamento_de_Servico/apps/profissionais/models/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from apps.empresas.models import Empresa
+from apps.servicos.models import Servico
+
+# Create your models here.
+class Profissional(models.Model):
+    nome = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=100, blank=True)
+    empresa = models.ForeignKey(
+        Empresa,
+        on_delete=models.CASCADE,
+        related_name='profissionais'
+    )
+
+    # Profissional para atender vários serviços
+    servicos = models.ManyToManyField(
+        Servico,
+        related_name='profissionais',
+        blank=True
+    )
+
+    ativo = models.BooleanField(default=True)
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.nome}" ({self.empresa.nome})
