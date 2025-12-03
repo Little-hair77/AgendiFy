2b98f212b90bcd940ff53d93661e676847cf8339
HigoPereira <higopereiraads@gmail.com>
2025-12-03 02:16:45 -0300

    integrando conteúdo aos admin

--- Agendamento_de_Servico/apps/usuarios/admin.py
+++ Agendamento_de_Servico/apps/usuarios/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
 # Register your models here.
+class CustomUsuarioAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informações adicionais', {
+            'fields': ('tipo_pessoa', 'telefone', 'data_nascimento', 'endereco', 'cpf', 'cnpj')
+        }),
+    )
